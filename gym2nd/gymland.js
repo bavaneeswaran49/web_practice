d092886218c0b7c0b658fd150de6017ee767777c
Bavaneeswar K <bavaneeswaran49@gmail.com>
2025-07-31 10:42:37 +0530

    Add files via upload
    gym website landing full page using HTML and CSS

--- gym2nd/gymland.js
+++ gym2nd/gymland.js
@@ -1,2 +1,55 @@
 // var a="welcome to our virtual gym";
 // window.alert(a);
+//window.alert("welcome to my fitness web page")
+
+
+
+
+//var a=10;
+//var b=30;
+//arthematic operators
+
+//console.log(a+b);          //add
+//console.log(a-b);          //sub
+//console.log(a*b);          //multiple
+//console.log(a/b);          //div
+//console.log(a%b);          //modulos
+//console.log(a**b);         //power
+//a++                        //increment
+//b--                          //decriment
+//console.log(b**a);
+
+
+//comparision or relational operator
+
+// var a=22;
+// var b=25;
+// console.log(a==b);  //equal to
+// console.log(a!=b) //not equal
+// console.log(a===b);  //strictly equal
+// console.log(a!==b);  //strictly not equal
+// console.log(a>b);  //greater than
+// console.log(a>=b);  //greater than or equal to
+// console.log(a<b);  //less than
+// console.log(a<=b);  //less than are equal to
+
+// bitwise operator
+// var a=15;
+// var b=17;
+// console.log(a&b);//and
+// console.log(a|b);//or
+// console.log(a<<<b); //right shift with zero
+
+//logical operator
+// var z=32;
+// var x=32;
+// console.log(x<12&&z>32); //and
+// console.log(z==32||x==12); //or
+
+// assignment operator
+// var a=10; 
+// console.log( a=a+10);//+=
+// console.log(a=a-10);//-=
+// console.log(a=a*10);//*=
+// console.log(a=a/10);// /=quotients
+// console.log(a=a%10);// %= reminder
